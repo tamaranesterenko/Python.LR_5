32700dcd70ced846413f3a777b31308db0ed914e
tamaranesterenko <90053368+tamaranesterenko@users.noreply.github.com>
2021-10-17 18:06:17 +0300

    Add Zadanie_2

---
+++ Zadanie_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    a = float(input("Enter a:"))
+    b = float(input("Enter b:"))
+    c = float(input("Enter c:"))
+
+    if abs(a) > 4:
+        print("Module a is more than 4")
+    if abs(b) > 4:
+        print("Module b is more than 4")
+    if abs(c) > 4:
+        print("Module c is more than 4")
